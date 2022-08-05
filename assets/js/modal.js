90dcd057fd2098e9cc570d1504f756267a02b1b7
Simone Bortolin <me@simonebortolin.it>
2022-08-05 08:28:47 +0200

    update

---
+++ assets/js/modal.js
@@ -0,0 +1,24 @@
+var modals = document.querySelectorAll("div[data-modal]");
+var modalToogles = document.querySelectorAll("div[data-toogle=modal]");
+
+
+[...modals].forEach(modal => {
+    var closeBtns = modal.getElementsByClassName("close");
+    [...closeBtns].forEach(closeBtn => {
+        closeBtn.addEventListener("click", (event) => {
+            modal.style.display = "none";
+        });
+    })
+});
+
+[...modalToogles].forEach(toogle => {
+    var modal = document.querySelector(toogle.getAttribute('data-target'));
+    modal.style.display = "block";
+});
+
+window.addEventListener("click", function(event) {
+    if (modals.includes(event.target)) {
+        event.target.style.display = "none";
+    }
+});
+
