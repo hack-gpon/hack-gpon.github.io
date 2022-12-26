28a0aca6a0cb04a5f9250939cc83d8325fad5772
Simone Bortolin <simonebortolin@users.noreply.github.com>
2022-12-26 22:26:15 +0100

    migrate theme-switch to new version of jtd

--- assets/js/theme-switch.js
+++ assets/js/theme-switch.js
@@ -1,37 +1,16 @@
-if(localStorage.getItem("color-scheme") === null) {
-    const newColorScheme = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light";
-    const newTextScheme = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? "Light mode" : "Dark mode";
-    jtd.setTheme(newColorScheme);
-    localStorage.setItem("color-scheme",newColorScheme);
-    localStorage.setItem("text-scheme",newTextScheme);
-    window.addEventListener('load', function () {
-        toggleDarkMode.textContent = newTextScheme; 
-    });
-} else {
-    jtd.setTheme(localStorage.getItem("color-scheme"));
-    window.addEventListener('load', function () {
-        toggleDarkMode.textContent = localStorage.getItem("text-scheme"); 
-    });
+jtd.switchThemeButton = function(button, event) {
+    const themes = ["auto", "light", "dark"];
+    var currentTheme = jtd.getTheme();
+    var nextTheme = themes[(themes.indexOf(currentTheme)+1)%themes.length];
+    jtd.setTheme(nextTheme);
+    button.getElementsByTagName('svg')[0].getElementsByTagName('use')[0].setAttribute('href',`#svg-${nextTheme}`);
+    window.localStorage.setItem('theme', nextTheme);
 }
 
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
-    if(localStorage.getItem("color-scheme") === null) {
-        const newColorScheme = event.matches ? "dark" : "light";
-        const newTextScheme = event.matches ? "Light mode" : "Dark mode";
-        jtd.setTheme(newColorScheme);
-        toggleDarkMode.textContent = newTextScheme; 
-        localStorage.setItem("color-scheme",newColorScheme);
-        localStorage.setItem("text-scheme",newTextScheme);
-    }
-});
-
-
-const toggleDarkMode = document.querySelector('.js-toggle-dark-mode'); 
-jtd.addEvent(toggleDarkMode, 'click', function(){ 
-    const newColorScheme = jtd.getTheme() !== 'dark' ? "dark" : "light";
-    const newTextScheme = jtd.getTheme() !== 'dark' ? "Light mode" : "Dark mode";
-    jtd.setTheme(newColorScheme);
-    toggleDarkMode.textContent = newTextScheme; 
-    localStorage.setItem("color-scheme",newColorScheme);
-    localStorage.setItem("text-scheme",newTextScheme);
+jtd.onReady(() => {
+    theme = window.localStorage.getItem('theme');
+    if(theme == null) return;
+    jtd.setTheme(theme);
+    var buttons = [...document.getElementsByClassName("color-scheme-switch-theme-button")];
+    buttons.forEach(button => button.getElementsByTagName('svg')[0].getElementsByTagName('use')[0].setAttribute('href',`#svg-${theme}`));
 });
