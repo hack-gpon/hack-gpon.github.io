85d9e18b2170526ba902e0affb1c9886f4c042bb
Giovanni Condello <nanomad@users.noreply.github.com>
2023-05-28 16:37:43 +0200

    XLTF7267-BHA+: Document how to change the PLOAM password (#212)
    * Fix typo in the software version string
    
    * Add PLOAM password settings
    
    * Remove a wrong warning
    
    * Update max PLOAM length
    
    * Remove misleading warning
    
    * Update _ont_xgs/ont-hisense-ltf7267-bha+.md
    
    * Apply suggestions from code review
    
    * add nanomad js script
    
    * fix js
    
    * Add missing braces
    
    ---------
    
    Co-authored-by: Simone Bortolin <simonebortolin@users.noreply.github.com>

---
+++ assets/js/LTF7267-BHA-ploam.js
@@ -0,0 +1,28 @@
+function hexEncode(str){
+    var hex, i;
+
+    var result = "";
+    for (i=0; i<str.length; i++) {
+        hex = str.charCodeAt(i).toString(16);
+        result += hex.padStart(2, "0");
+    }
+
+    return result;
+}
+
+function hisensePloam(ascii_ploam) {
+    var hex_ploam = hexEncode(ascii_ploam);
+    var hex_padded_ploam = hex_ploam.padEnd(72, "0");
+    var array =[];
+    for (i = 0; i<9; i++) {
+        ploam_segment = hex_padded_ploam.slice(i*8, (i+1)*8);
+        new_ploam_segment = "";
+        for(j = 4; j>0; j--) {
+            new_ploam_segment = new_ploam_segment + ploam_segment.slice((j-1)*2, j*2);
+        }
+        if(new_ploam_segment !== "00000000") {
+            array.push("INT             CFG_ID_PON_REGISTRATION_ID"+i+"                                        = 0x"+new_ploam_segment+";");
+        }
+    }
+    return array.join("\n");
+}
